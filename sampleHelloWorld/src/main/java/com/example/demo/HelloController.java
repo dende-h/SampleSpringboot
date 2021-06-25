d2a6dc121e68c0a79aaae8b70ff6cb51993496bc
dende-h <miyuki_dende@yahoo.co.jp>
2021-06-25 16:23:14 +0900

    送信フォーム作成 受信した言葉を受け取るhtmlファイル作成

--- sampleHelloWorld/src/main/java/com/example/demo/HelloController.java
+++ sampleHelloWorld/src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,11 @@ public class HelloController {
 		model.addAttribute("title", "Hello Springboot!" );
 		return "index";
 	}
-
+	@RequestMapping(value = "/callResponce")
+	public ModelAndView responce(@RequestParam String name, @RequestParam String tweet) {
+		ModelAndView mav = new ModelAndView();//メソッドで引数を受け取りながらmavインスタンスを生成
+		mav.setViewName("callResponce");//インスタンスでsetViewNameメソッドでindex.htmlにアクセス
+		mav.addObject("responce", "こんにちは、" + name + "です " + "座右の銘は" + tweet);//addObjectメソッドでresponceをキーにして値を返す
+		return mav;
+	}
 }
